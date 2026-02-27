469db458a659e5edb67f252ad1c17e39f076faf4
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-02-27 11:09:00 +0100

    changes

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
 -- script to list all databses
-SELECT s.title, sg.genre_id
-FROM tv_shows AS s
-INNER JOIN tv_show_genres AS sg
-    ON s.id = sg.tv_show_id
-WHERE sg.genre_id IS NOT NULL
-ORDER BY s.title ASC, sg.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
