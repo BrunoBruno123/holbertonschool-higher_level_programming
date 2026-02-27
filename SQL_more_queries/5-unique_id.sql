e0928f7733e4f757e7f07346dd1af7f451580cce
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-02-27 10:54:38 +0100

    changes

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- script to list all databses
+-- Create table unique_id if it does not exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT NOT NULL DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
