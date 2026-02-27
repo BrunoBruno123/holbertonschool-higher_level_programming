5440aa09ade5bbcf060ebf3658105dd06238e97d
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-02-27 11:02:19 +0100

    changes

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,2 @@
 -- script to list all databses
-USE hbtn_0d_usa;
-
-SELECT *
-FROM cities
-WHERE state_id IN (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
-ORDER BY id ASC;
+SELECT * FROM cities WHERE state_id = ( SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
