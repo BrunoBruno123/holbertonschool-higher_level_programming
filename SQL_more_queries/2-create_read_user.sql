5bec5140e07996361168a6a8ee4207181f33edc9
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-02-27 10:51:08 +0100

    changes

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- script to list all databses
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
